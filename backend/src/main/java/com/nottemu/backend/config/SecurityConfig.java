cf3547031f8cbf6fd1ad3bad2258789c900e124f
oggy-huuanh <oggyoggt@gmail.com>
2026-05-15 15:09:31 +0300

    cors config fixx

--- backend/src/main/java/com/nottemu/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/nottemu/backend/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:5173",
                 "http://localhost:3000",
-                "https://slembaby2910.github.io"));
+                "https://oggy-huuanh.github.io"));
 
         configuration.setAllowedMethods(List.of(
                 "GET",
